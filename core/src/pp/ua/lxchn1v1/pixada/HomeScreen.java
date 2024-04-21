401f1f8559f7b0d283bdb986dd56020c78972389
andrewtheyoung <andrew.young@ukr.net>
2024-04-21 10:00:35 +0300

    Added preferences, packr configs

--- core/src/pp/ua/lxchn1v1/pixada/HomeScreen.java
+++ core/src/pp/ua/lxchn1v1/pixada/HomeScreen.java
@@ -5,16 +5,26 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
+
 
 public class HomeScreen implements Screen {
     final Pixada app;
     private Stage stage;
+    private Table table;
 
     public HomeScreen(final Pixada app)
     {
         this.app = app;
         stage = new Stage(new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
+        Gdx.input.setInputProcessor(stage);
+    
+        table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
     }
 
     @Override
@@ -34,12 +44,14 @@ public class HomeScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-        
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
--- core/src/pp/ua/lxchn1v1/pixada/Pixada.java
+++ core/src/pp/ua/lxchn1v1/pixada/Pixada.java
@@ -1,17 +1,30 @@
 package pp.ua.lxchn1v1.pixada;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 
 public class Pixada extends Game {
-
+	public SpriteBatch batch;
+	public BitmapFont font;
+	public Preferences prefs;
+	
+	@Override
 	public void create() {
+		prefs = Gdx.app.getPreferences("Pixada");
+
 		this.setScreen(new HomeScreen(this));
 	}
 
+	@Override
 	public void render() {
 		super.render();
 	}
 
+	@Override
 	public void dispose() {
 
 	}
